1df4fa9c5b85850e6b7d0c1b912cc2f78a00bc13
192.168.121.012 <192.168.121.012@server>
2016-05-26 21:23:30 +0700

    Menambahkan class AddAndSubInterface.java

--- src/rkppl/kelompok3/tubes/AddAndSubInterface.java
+++ src/rkppl/kelompok3/tubes/AddAndSubInterface.java
@@ -1,5 +1,11 @@
 package rkppl.kelompok3.tubes;
 
 public interface AddAndSubInterface {
-
+    public int add(int n1, int n2){
+    return 0;
+  }
+  
+  public int sub(int n1, int n2){
+    return 0;
+  }
 }
